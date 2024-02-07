0115696a88c9644683d3c6bbd63bb3d1b255efea
Jimmy Torres <38924150+yimmytor@users.noreply.github.com>
2024-02-07 11:48:02 -0600

    Commit inicial

---
+++ APITest/Services/CategoriesService.cs
@@ -0,0 +1,32 @@
+﻿using Entities;
+using Business;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class CategoriesService : ICategoriesService
+    {
+        private CategoryResponse ConvertToResponse(Category category)
+        {
+            CategoryResponse categoryResponse = category.ToCategoryResponse();
+
+            return categoryResponse;
+        }
+
+        public List<CategoryResponse> GetAll()
+        {
+            return CategoryB.GetAll().Select(category => ConvertToResponse(category)).ToList();
+        }
+
+        public CategoryResponse? GetById(int? categoryId)
+        {
+            if (categoryId == 0 || categoryId == null)
+            {
+                return null;
+            }
+
+            return CategoryB.GetById(categoryId).ToCategoryResponse();
+        }
+    }
+}
---
+++ APITest/Services/ProductsService.cs
@@ -0,0 +1,52 @@
+﻿using Entities;
+using Business;
+using ServiceContracts;
+using ServiceContracts.DTO;
+
+namespace Services
+{
+    public class ProductsService : IProductsService
+    {
+        private ProductResponse ConvertToResponse(Product product)
+        {
+            ProductResponse productResponse = product.ToProductResponse();
+
+            return productResponse;
+        }
+
+        public List<ProductResponse> GetAll()
+        {
+            return ProductB.GetAll().Select(product => ConvertToResponse(product)).ToList();
+        }
+
+        public ProductResponse? GetById(int? productId)
+        {
+            if (productId == 0 || productId == null)
+            {
+                return null;
+            }
+
+            return ProductB.GetById(productId).ToProductResponse();
+        }
+
+        public List<ProductResponse> SearchByText(string? searchText)
+        {
+            if (String.IsNullOrEmpty(searchText))
+            {
+                return new List<ProductResponse>() { };
+            }
+
+            return ProductB.SearchByText(searchText).Select(product => ConvertToResponse(product)).ToList();
+        }
+
+        public List<ProductResponse> SearchByCategory(int? categoryId)
+        {
+            if (categoryId == 0 || categoryId == null)
+            {
+                return new List<ProductResponse>(){ };
+            }
+
+            return ProductB.SearchByCategory(categoryId).Select(product => ConvertToResponse(product)).ToList();
+        }
+    }
+}
