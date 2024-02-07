dfe86c0fc72369f3267268e558f6e636ddb27ff9
Jimmy Torres <38924150+yimmytor@users.noreply.github.com>
2024-02-07 11:42:31 -0600

    Commit inicial

---
+++ APITest/Business/CategoryB.cs
@@ -0,0 +1,18 @@
+﻿using Entities;
+using DataAccess;
+
+namespace Business
+{
+    public static class CategoryB
+    {
+        public static List<Category> GetAll()
+        {
+            return CategoryDA.GetAll();
+        }
+
+        public static Category? GetById(int? categoryId)
+        {
+            return CategoryDA.GetById(categoryId);
+        }
+    }
+}
---
+++ APITest/Business/ProductB.cs
@@ -0,0 +1,28 @@
+﻿using Entities;
+using DataAccess;
+
+namespace Business
+{
+    public static class ProductB
+    {
+        public static List<Product> GetAll()
+        {
+            return ProductDA.GetAll();
+        }
+
+        public static List<Product> SearchByText(string? searchtext)
+        {
+            return ProductDA.SearchByText(searchtext);
+        }
+
+        public static List<Product> SearchByCategory(int? categoryId)
+        {
+            return ProductDA.SearchByCategory(categoryId);
+        }
+
+        public static Product? GetById(int? productId)
+        {
+            return ProductDA.GetById(productId);
+        }
+    }
+}
