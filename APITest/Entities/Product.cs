8907c952b25da97169376a11ec6fb440ba77540f
Jimmy Torres <38924150+yimmytor@users.noreply.github.com>
2024-02-07 11:46:19 -0600

    Commit inicial

---
+++ APITest/Entities/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public Category? Category { get; set; }
+        public string? Sku { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<ProductImage>? Images { get; set; }
+        public List<string>? Tags { get; set; }
+    }
+}
---
+++ APITest/Entities/ProductImage.cs
@@ -0,0 +1,9 @@
+﻿namespace Entities
+{
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public string? Url { get; set; }
+        public int Type { get; set; }         
+    }
+}
